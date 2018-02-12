f2b98b9cb58109b2feebb9d6fa4bae66e7f0c713
B Hari prasad <39781@hexaware.com>
2018-02-12 09:47:17 +0530

    first commit

--- config.js
+++ config.js
@@ -3,61 +3,61 @@ var config ={
 	laptop:{
 		ACER:{
 			cost : 28000,
-			Ram : 1GB,
-			Processor:dualCore,
-			HDD: 1TB,
-			Size :15inches
+			Ram : "1GB",
+			Processor:"dualCore",
+			HDD: "1TB",
+			Size :"15inches"
 		},
 		HP:{
 			cost : 35000,
-			Ram : 1GB,
-			Processor:dualCore,
-			HDD: 1TB,
-			Size :15inches
+			Ram : "1GB",
+			Processor:"dualCore",
+			HDD: "1TB",
+			Size :"15inches"
 		},
 		DELL:{
-			cost : 38000
-			Ram : 1GB,
-			Processor:dualCore,
-			HDD: 1TB,
-			Size :15inches
-		}
+			cost : 38000,
+			Ram : "1GB",
+			Processor:"dualCore",
+			HDD: "1TB",
+			Size :"15inches"
+		},
 		Lenova:{
-			cost: 25000
-			Ram : 1GB,
-			Processor:dualCore,
-			HDD: 1TB,
-			Size :15inches
+			cost: 25000,
+			Ram : "1GB",
+			Processor:"dualCore",
+			HDD: "1TB",
+			Size :"15inches"
 		}
 	},
 	PC:{
 		ACER:{
 			cost : 28000,
-			Ram : 1GB,
-			Processor:dualCore,
-			HDD: 1TB,
-			Size :15inches
+			Ram : "1GB",
+			Processor:"dualCore",
+			HDD: "1TB",
+			Size :"15inches"
 		},
 		HP:{
 			cost : 35000,
-			Ram : 1GB,
-			Processor:dualCore,
-			HDD: 1TB,
-			Size :15inches
+			Ram : "1GB",
+			Processor:"dualCore",
+			HDD: "1TB",
+			Size :"15inches"
 		},
 		DELL:{
-			cost : 38000
-			Ram : 1GB,
-			Processor:dualCore,
-			HDD: 1TB,
-			Size :15inches
-		}
+			cost : 38000,
+			Ram : "1GB",
+			Processor:"dualCore",
+			HDD: "1TB",
+			Size :"15inches"
+		},
 		Lenova:{
-			cost: 25000
-			Ram : 1GB,
-			Processor:dualCore,
-			HDD: 1TB,
-			Size :15inches
+			cost: 25000,
+			Ram : "1GB",
+			Processor:"dualCore",
+			HDD: "1TB",
+			Size :"15inches"
 		}
 	}
 }
